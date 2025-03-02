20649761a130aaa666e15196ac745d60163f9b8a
rhettrina <michael18ricafrente@gmail.com>
2025-03-02 21:09:27 +0800

    nawala yun animation na ginawa ko kaya inulit ko

--- src/components/headers/LandingHeader.jsx
+++ src/components/headers/LandingHeader.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
 const LandingHeader = () => {
   const { isAuthenticated } = useAuth();
   const [isScrolled, setIsScrolled] = useState(false);
-  const location = useLocation();
 
-  // Function to handle scroll behavior
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
     setIsScrolled(currentScrollY > 50);
@@ -18,32 +16,31 @@ const LandingHeader = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const isSpecialRoute = location.pathname === '/news&events' || location.pathname === '/about';
-
   return (
     <header
-      className={`w-full fixed z-50 transition-all duration-300 ${isScrolled ? 'bg-[#1C1B19] shadow-lg' : 'bg-transparent shadow-none'}`}
+      className={`w-full fixed z-50 transition-all duration-300 ${isScrolled ? 'bg-[#1C1B19] shadow-lg' : 'bg-transparent shadow-none'
+        }`}
     >
-      <div className="flex w-auto h-10 justify-between px-5">
-        <span className={`${isScrolled ? 'text-white' : isSpecialRoute ? 'text-black' : 'text-white'} text-xs h-fit w-fit my-auto`}>
+      <div className="flex w-auto h-7 justify-between px-5">
+        <span className="text-white text-xs h-fit w-fit my-auto">
           Open Daily 9:00am-5:00pm, Monday-Friday, Closed During Holidays
         </span>
-        <div className="w-auto flex">
+        <div className="w-auto">
           {isScrolled && (
-            <div className="inline-block animate-none my-auto">
+            <div className="inline-block animate-slide-in-right">
               <NavLink to="/" className="mx-2">
                 <span className="text-white text-xs my-auto cursor-pointer">Home</span>
               </NavLink>
               <NavLink to="/news&events" className="mx-2">
                 <span className="text-white text-xs my-auto cursor-pointer">News & Events</span>
               </NavLink>
-              <NavLink to="/about" className="mx-2 h-full">
+              <NavLink to="/about" className="mx-2">
                 <span className="text-white text-xs my-auto cursor-pointer">About</span>
               </NavLink>
             </div>
           )}
-          <NavLink to="/login" className="mx-2 my-auto">
-            <span className={`${isScrolled ? 'text-white' : isSpecialRoute ? 'text-black' : 'text-white'} text-xs my-auto cursor-pointer`}>Login</span>
+          <NavLink to="/login" className="mx-2">
+            <span className="text-white text-xs my-auto cursor-pointer">Login</span>
           </NavLink>
         </div>
       </div>
