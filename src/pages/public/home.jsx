94082416efc4f8c959a235499de5e35d42db793a
k3mosh <cocsmurftzy@gmail.com>
2025-03-02 16:28:32 +0800

    homepage second part done

--- src/pages/public/home.jsx
+++ src/pages/public/home.jsx
@@ -137,10 +137,41 @@ const home = () => {
         </div>
           
       </div>
-      <div className="w-auto z mx-auto pt-7 min-h-screen bg-cover bg-center bg-no-repeat overflow-hidden"
-      style={{ backgroundImage: "url('./assets/bghome2.png')" }}>
+      <div className="w-auto max-h-full mx-auto pt-7 min-h-screen bg-cover bg-center bg-no-repeat overflow-hidden" 
+          style={{ backgroundImage: "url('./assets/bghome2.png')" }}>
+          <div className="w-auto h-150 mt-35 ml-35 mr-0 flex flex-col items-start">
 
+            <div className="w-auto h-auto flex items-center">
+              <h1 className="w-7 h-0.5 bg-[#636363] mr-2"></h1>
+              <span className="text-[#636363]">About Museo Bulawan</span>
+            </div>
+
+            <div className="w-full h-full flex justify-between items-start mt-8 gap-12">
+              <div className="w-1/2 h-auto pt-6">
+                <span className="text-4xl leading-snug tracking-wide font-[HinaMincho]">
+                  <span className="text-5xl font-medium">
+                    Museo Bulawan
+                  </span>, known as the “Golden Museum,” is the leading and the most viewer-friendly community museum that serves as the nerve center for education and communication of the rich cultural, artistic and natural heritage and history of Camarines Norte. It is the ultimate medium for preservation and exhibition of significant objects that strengthen the values of the people and deepen their patriotism and sense of identity.
+                </span>
+
+                <div className='w-auto mt-12 flex items-center'>
+                  <span className='text-xl font-semibold'>Learn More </span>
+                  <img src="/assets/arrow-right.png" alt="arrow" className="ml-2 w-8" /> {/* Adjust the width of the image */}
+                </div>
+              </div>
+
+              <div className="w-1/2 h-full">
+                <img src="/assets/dhome2.png" alt="Museo Bulawan" className="w-full h-full" />
+              </div>
+
+            </div>
+
+           
+
+          </div>
       </div>
+
+
     </>
   );
 };
