fa0516f20ebb254100d8e20ad2e96b97661715dd
hachisnail <jeffersontalagtag06@gmail.com>
2025-03-08 02:53:58 +0800

    init landing footer 40% done

--- src/components/footers/LandingFooter.jsx
+++ src/components/footers/LandingFooter.jsx
@@ -8,8 +8,33 @@ const LandingFooter = () => {
 
   return (
     <>
-    <div className='w-screen h-50 bg-amber-300'>
+    <div className='w-screen h-70 bg-[#1C1B19] p-10'>
+      <div className='w-full h-full flex flex-col'>
+        <div className='w-full h-1/3 flex'>
+          <img src="./assets/LOGO.png" alt="Museo Bulawan Logo" />
+          <span className='w-auto h-auto font-bold text-4xl text-white my-auto ml-2'>MUSEO BULWAN</span>
+        </div>
+        <div className='w-full h-2/3 flex space-x-2'>
+          <div className='w-[40%] h-full'>
+            <div className='w-full h-[55%] flex'>
+              <span className='text-white text-2xl font-serif my-auto mx-3'>
+              Helping us raise awareness regarding Camnortenos identity is crucial, and your support can make a significant difference.
+              </span>
+            </div>
+            <div className='w-full h-[45%]'>
+              <span className='w-auto my-auto mx-2 text-white'>
+              We gratefully accept donations or just lending of your artifact will greatly help us.
+              </span>
+            </div>
+          </div>
+          <div className='w-[30%] h-full'>
 
+          </div>
+          <div lassName='w-[30%] h-full'>
+
+          </div>
+        </div>
+      </div>
     </div>
     </>
   );
